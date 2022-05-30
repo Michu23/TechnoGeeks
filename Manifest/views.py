359fcc6e784f0ed95ebc7a8cf7231b080f02b1e6
sersha01 <skperson01@gmail.com>
2022-05-30 18:55:20 +0530

    2 else case added

--- Manifest/views.py
+++ Manifest/views.py
@@ -10,21 +10,30 @@ from Manifest.models import Manifest
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def getTaskslist(request):
-    if request.user.is_staff:
-        student = Student.objects.get(id=request.data['id'])
+    print(request.user.is_student)
+    if request.user.is_staff or request.user.is_student:
+        if request.user.is_staff:
+            student = Student.objects.get(id=request.data['id'])
+        else:
+            student = request.user.student
         manifests = Manifest.objects.filter(student_name=student)
         for manifest in manifests:
             manifest.pending = Tasks.objects.filter(week=manifest, status=False).count()
             manifest.reviews = Review.objects.filter(manifest=manifest)
             manifest.save()
         serializer = StudentTasklistSerializer(manifests, many=True).data
+        print('done')
         return Response(serializer)
+    else:
+        return Response({'error': 'You are not allowed to perform this action'})
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def getManifest(request):
-    if request.user.is_staff:
+    if request.user.is_staff or request.user.is_student:
         manifest = Manifest.objects.get(id=request.data['id'])
         manifest.tasks = Tasks.objects.filter(week=manifest)
         serializer = ManifestTaskSerealizer(manifest).data
         return Response(serializer)
+    else:
+        return Response({'error': 'You are not allowed to perform this action'})
