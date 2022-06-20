3e75b446ef05a83dbce8d01bacf69222a0b03d48
sersha01 <skperson01@gmail.com>
2022-06-20 17:33:08 +0530

    done

--- backend/settings.py
+++ backend/settings.py
@@ -133,7 +133,7 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'technogeeks',
+        'NAME': 'NewData',
         'USER': 'postgres',
         'PASSWORD': config('db_password'),
         'HOST': 'localhost',
@@ -186,12 +186,6 @@ MEDIA_ROOT = BASE_DIR/ 'Media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('email')
-EMAIL_HOST_PASSWORD = config('emailpass')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
