6082113268373842ed906a3785c79eac96931bcb
sersha01 <skperson01@gmail.com>
2022-06-26 08:52:56 +0530

    signup ok

--- Batch/admin.py
+++ Batch/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Batch, Branch, Group,Location
+from .models import Batch, Branch, Group
 # Register your models here.
 
 admin.site.register(Batch)
 admin.site.register(Group)
-admin.site.register(Location)
 admin.site.register(Branch)
--- User/serializer.py
+++ User/serializer.py
@@ -1,17 +1,17 @@
 from datetime import timedelta
 from rest_framework import serializers
 from .models import Department, User, Domain, Profile, Notification
-from Admin.models import Advisor
+from Admin.models import Advisor, Location
 from Student.models import Student
 from Manifest.models import Manifest
-from Batch.models import Batch,Location,Branch
+from Batch.models import Batch, Branch
 
 class UserSerealizer(serializers.ModelSerializer):
     batch = serializers.CharField(write_only=True)
+    location = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'email', 'is_staff', 'is_student', 'batch']
-
+        fields = ['id', 'username', 'password', 'email', 'is_staff', 'is_student', 'batch', 'location']
     def create(self, validated_data):
         user = User.objects.create(
             username=validated_data['username'],email=validated_data['email']
@@ -23,14 +23,16 @@ class UserSerealizer(serializers.ModelSerializer):
             user.is_staff = True
             user.save()
             profile = Profile.objects.create()
-            Advisor.objects.create(user=user, profile=profile)
+            location = Location.objects.get(id=validated_data['location'])
+            Advisor.objects.create(user=user, profile=profile, location=location)
         else:
             user.department = Department.objects.get(name='Student')
             user.is_student = True
             user.save()
             profile = Profile.objects.create()
             batch = Batch.objects.get(id = validated_data['batch'])
-            student = Student.objects.create(user=user, profile=profile, batch=batch)
+            branch = Branch.objects.get(id = validated_data['location'])
+            student = Student.objects.create(user=user, profile=profile, batch=batch, branch=branch)
             day = student.batch.created_at + timedelta(days=7) if student.batch.created_at.strftime('%a') == "Sun" else student.batch.created_at + timedelta(days=8)
             Manifest.objects.create(title = 'Week 01',student_name=student, next_review=day)
         return user
